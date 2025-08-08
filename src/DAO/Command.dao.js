28e3b245de913fcdbbb7130ab84a37ea736b3ade
Jcvanz-wehsoft <julio.vanz@wehsoft.com>
2025-08-08 10:41:34 -0300

    Ajustes de exibição das mensagens

--- src/DAO/Command.dao.js
+++ src/DAO/Command.dao.js
@@ -4,7 +4,7 @@ const createCommand = async (commandData) => {
     try {
         return await Command.create(commandData);
     } catch (error) {
-        throw new Error(`Erro ao criar comando: ${error.message}`);
+        throw new Error(`Erro ao criar comanda: ${error.message}`);
     }
 }
 
@@ -12,7 +12,7 @@ const getCommands = async () => {
     try {
         return await Command.find().populate('command').populate('commandProducts.products');
     } catch (error) {
-        throw new Error(`Erro ao listar comandos: ${error.message}`);
+        throw new Error(`Erro ao listar comandas: ${error.message}`);
     }
 }
 
@@ -20,7 +20,7 @@ const getCommandById = async (commandId) => {
     try {
         return await Command.findById(commandId).populate('command').populate('commandProducts.products');
     } catch (error) {
-        throw new Error(`Erro ao buscar comando: ${error.message}`);
+        throw new Error(`Erro ao buscar comanda: ${error.message}`);
     }
 }
 
@@ -28,7 +28,7 @@ const updateCommand = async (commandId, updateData) => {
     try {
         return await Command.findByIdAndUpdate(commandId, updateData, { new: true }).populate('command').populate('commandProducts.products');
     } catch (error) {
-        throw new Error(`Erro ao atualizar comando: ${error.message}`);
+        throw new Error(`Erro ao atualizar comanda: ${error.message}`);
     }
 }
 
@@ -36,7 +36,7 @@ const deleteCommand = async (commandId) => {
     try {
         return await Command.findByIdAndDelete(commandId);
     } catch (error) {
-        throw new Error(`Erro ao deletar comando: ${error.message}`);
+        throw new Error(`Erro ao deletar comanda: ${error.message}`);
     }
 }
 
