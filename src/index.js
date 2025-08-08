b934d85396345b0dc4aba69b1e3f250d2926c89c
Jcvanz-wehsoft <julio.vanz@wehsoft.com>
2025-08-08 10:39:38 -0300

    Criado rotas para acessar o swagger

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const connectDatabase = require('./config/database');
 
-const productRoutes = require('./routes/products.routes');
-const userRoutes = require('./routes/users.routes');
-const commandRoutes = require('./routes/command.routes');
-
 require('dotenv').config();
 connectDatabase();
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger');
+
+const productRoutes = require('./routes/products.routes');
+const userRoutes = require('./routes/users.routes');
+const commandRoutes = require('./routes/commands.routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get('/api-docs.json', (_req, res) => res.json(swaggerSpec));
+app.use('/api-docs', swaggerUi.serveFiles(swaggerSpec, {}), swaggerUi.setup(swaggerSpec, { explorer: true }));
+
 app.use('/produtos', productRoutes);
 app.use('/clientes', userRoutes);
-app.use('/comanda', commandRoutes);
+app.use('/comandas', commandRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
