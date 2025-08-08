8d26f7359961d30d632e6519991d9830c5a339e7
Jcvanz-wehsoft <julio.vanz@wehsoft.com>
2025-08-08 11:03:56 -0300

    Alterado nome da variável

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 async function connectDatabase() {
     try {
-        await mongoose.connect(process.env.SECRET_API);
+        await mongoose.connect(process.env.DB_CONNECTION);
         console.log("Database Conectado");
     } catch (error) {
         console.error("Erro ao conectar banco de dados:", error);
